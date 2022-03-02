e4a16e8e728e48958954803ff7d8799831df61c7
Edu Caribe <edu.caribe@hotmail.com>
2022-03-02 00:06:36 -0300

    refactor sphere creation to abstract implementation

--- src/main.py
+++ src/main.py
@@ -8,55 +8,31 @@ from OpenGL.GLU import *
 from OpenGL.GLUT import *
 
 
-def create_sphere(textureId):
-    qobj = gluNewQuadric()
-    gluQuadricTexture(qobj, GL_TRUE)
-    glEnable(GL_TEXTURE_2D)
-    glBindTexture(GL_TEXTURE_2D, textureId)
-    gluSphere(qobj, 1, 50, 50)
-    gluDeleteQuadric(qobj)
-    glDisable(GL_TEXTURE_2D)
-
-
 def main():
     pygame.init()
 
+    # Start display with default settings
     start_display()
-
     # Allows press and hold of buttons
     pygame.key.set_repeat(1, 10)
     # Set's initial zoom so we can see globe
     glTranslatef(0.0, 0.0, -5)
 
-    position = {
+    cam_position = {
         "x": 0,
         "y": 0
     }
 
-    textureId = texture.read('world.jpg')
+    system = SolarSystem()
 
-    now = 0
     while True:
-        position = events.handle(position)
+        cam_position = events.handle(cam_position)
 
         # Creates Sphere and wraps texture
         glEnable(GL_DEPTH_TEST)
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
-        glPushMatrix()  # create first matrix
-        glRotatef(now, 0, 0, -1)
-        now += 2
-        if (now > 360):
-            now -= 360
-
-        create_sphere(textureId)
-        glPopMatrix()
-
-        glPushMatrix()
-        glTranslatef(10, 10, 0)
-
-        create_sphere(textureId)
-        glPopMatrix()
+        system.draw()
 
         # Displays pygame window
         pygame.display.flip()
@@ -68,6 +44,50 @@ def start_display():
     pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
     pygame.display.set_caption('Solar System')
     gluPerspective(40, (display[0]/display[1]), 0.1, 50.0)
+
+
+class Orb:
+    def __init__(self, rotation, scale, start_position, has_movement, texture_name):
+        self.current_rotation = 0
+        self.rotation = rotation
+        self.scale = scale
+        self.start_position = start_position
+        self.has_movement = has_movement
+        self.texture_id = texture.read(texture_name)
+
+
+class SolarSystem:
+    def __init__(self):
+        self.orbs = [
+            Orb(2, 1, 0, True, 'world.jpg'),
+            Orb(2, 1, 10, True, 'world.jpg'),
+            Orb(2, 1, 20, True, 'world.jpg')
+        ]
+
+    def draw(self):
+        for orb in self.orbs:
+            self.create_orb(orb)
+
+    def create_orb(self, orb):
+        glPushMatrix()
+
+        glTranslatef(orb.start_position, orb.start_position, 0)
+
+        glRotatef(orb.current_rotation, 0, 0, -1)
+
+        orb.current_rotation += orb.rotation
+        if (orb.current_rotation > 360):
+            orb.current_rotation -= 360
+
+        quadric = gluNewQuadric()
+        gluQuadricTexture(quadric, GL_TRUE)
+        glEnable(GL_TEXTURE_2D)
+        glBindTexture(GL_TEXTURE_2D, orb.texture_id)
+        gluSphere(quadric, 1, 50, 50)
+        gluDeleteQuadric(quadric)
+        glDisable(GL_TEXTURE_2D)
+
+        glPopMatrix()
 
 
 if __name__ == "__main__":
