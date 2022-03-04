b18ded0143ca5656ce5b9ed844ea305ef346c815
Edu Caribe <edu.caribe@hotmail.com>
2022-03-04 08:52:23 -0300

    add file resolve method description

--- src/file.py
+++ src/file.py
@@ -2,4 +2,7 @@ import os
 
 
 def resolve(filename):
+    """
+    Resolves file path independent of path of command execution 
+    """
     return '{0}\\assets\{1}'.format(os.path.dirname(os.path.abspath(__file__)), filename)
